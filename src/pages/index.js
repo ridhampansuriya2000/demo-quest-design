b2ef563aafd37a7321f4b50e5d900640e1cba24b
ridhampansuriya2000 <ridhampansuriya2000@gmail.com>
2024-04-10 17:54:58 +0530

    remove padding from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home(pageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${inter.className}`}>
         <Dashboard />
       </main>
       <div>{pageProps.children}</div>
